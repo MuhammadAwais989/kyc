6eac3c75955c6eb6fcf9b5afbcb71b9f9f80c563
MuhammadAwais989 <officialawais989@gmail.com>
2026-03-19 14:02:17 +0500

    updates

--- routes/verification.routes.js
+++ routes/verification.routes.js
@@ -5,6 +5,7 @@ const { redirectToDidit } = require("../controllers/verification.controller");
 const { diditWebhook } = require("../webhooks/didit.webhook");
 
 router.post("/verify", redirectToDidit);        
+router.get("/verify", redirectToDidit);        
 router.post("/webhook/didit", diditWebhook);   
 router.get("/webhook/didit", diditWebhook);   
 
