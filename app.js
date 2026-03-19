8194f6890a67971c29071335029754259399cd81
MuhammadAwais989 <officialawais989@gmail.com>
2026-03-19 09:50:48 +0500

    updates

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*"
+    }
+));
 
 const verificationRoutes = require("./routes/verification.routes");
 
